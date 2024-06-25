d8d5c00570370e5a3f66caf126fa1a4c558570d1
Miit Daga <miitcodes27@gmail.com>
2024-06-25 13:34:22 +0530

    add(frontend): added API functions for user authentication and task management

---
+++ src/utils/apis.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+const API_URL = "http://localhost:3000";
+
+export const register = (userData) => {
+  return axios.post(`${API_URL}/signup`, userData, {
+    withCredentials: true,
+  });
+};
+
+export const login = (loginData) => {
+  return axios.post(`${API_URL}/login`, loginData, { withCredentials: true });
+};
+
+export const logOutUser = () => {
+  return axios.get(`${API_URL}/logout`, { withCredentials: true });
+};
+
+export const fetchTasks = () => {
+  return axios.get(`${API_URL}/tasks`);
+};
+
+export const getCompletedTasks = () => {
+  return axios.get(`${API_URL}/tasks/completed`);
+};
+
+export const getIncompletedTasks = () => {
+  return axios.get(`${API_URL}/tasks/incompleted`);
+};
+
+export const createTask = (taskData) => {
+  return axios.post(`${API_URL}/task`, taskData);
+};
+
+export const updateTask = (id, updatedTask) => {
+  return axios.put(`${API_URL}/updatecontent/${id}`, updatedTask);
+};
+
+export const updateTaskStatus = (id) => {
+  return axios.put(`${API_URL}/updatestatus/${id}`);
+};
+
+export const deleteTask = (id) => {
+  return axios.delete(`${API_URL}/task/${id}`);
+};

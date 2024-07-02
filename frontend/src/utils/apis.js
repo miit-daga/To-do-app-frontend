95f79e283f6088cdab41dbf8b45bb760e0e7ff05
Miit Daga <miitcodes27@gmail.com>
2024-07-02 13:11:54 +0530

    fix(frontend): added functionality for sending cookie with every request

--- frontend/src/utils/apis.js
+++ frontend/src/utils/apis.js
@@ -17,29 +17,29 @@ export const logOutUser = () => {
 };
 
 export const fetchTasks = () => {
-  return axios.get(`${API_URL}/tasks`);
+  return axios.get(`${API_URL}/tasks`,{ withCredentials: true });
 };
 
 export const getCompletedTasks = () => {
-  return axios.get(`${API_URL}/tasks/completed`);
+  return axios.get(`${API_URL}/tasks/completed`,{ withCredentials: true });
 };
 
 export const getIncompletedTasks = () => {
-  return axios.get(`${API_URL}/tasks/incompleted`);
+  return axios.get(`${API_URL}/tasks/incompleted`,{ withCredentials: true });
 };
 
 export const createTask = (taskData) => {
-  return axios.post(`${API_URL}/task`, taskData);
+  return axios.post(`${API_URL}/task`, taskData,{ withCredentials: true });
 };
 
 export const updateTask = (id, updatedTask) => {
-  return axios.put(`${API_URL}/updatecontent/${id}`, updatedTask);
+  return axios.put(`${API_URL}/updatecontent/${id}`, updatedTask,{ withCredentials: true });
 };
 
 export const updateTaskStatus = (id) => {
-  return axios.put(`${API_URL}/updatestatus/${id}`);
+  return axios.put(`${API_URL}/updatestatus/${id}`,{ withCredentials: true });
 };
 
 export const deleteTask = (id) => {
-  return axios.delete(`${API_URL}/task/${id}`);
+  return axios.delete(`${API_URL}/task/${id}`,{ withCredentials: true });
 };
